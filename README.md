435f297d2240e0b3d3e5b1fce3b143332de982e9
rkgallaway <rkgallaway@msn.com>
2019-01-23 08:15:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+![CF](http://i.imgur.com/7v5ASc8.png)
+## cf401js-api-server
+* Basic Api Server as available to 401js 401d28 as of 1/21/19
+
+#### Props to John Cokos, Insturctor
+
+Enjoy your experience!
---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongooseOptions = {
+  useNewUrlParser:true,
+  useCreateIndex: true,
+};
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/template`
+  , mongooseOptions);
+
+require('./src/app.js').start(process.env.PORT || 3000);
